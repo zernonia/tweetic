3cf73ce0d974dbc8fe6f265fb06420c02158e38b
zernonia <zernonia@gmail.com>
2022-05-11 12:14:07 +0800

    construct html, add css

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="p-8">
     <!-- <NuxtWelcome /> -->
 
     <div v-for="tweet in tweetsHtml">
-      <div v-if="tweet.length" v-html="tweet"></div>
+      <div class="mb-6" v-if="tweet.length" v-html="tweet"></div>
     </div>
   </div>
 </template>
@@ -31,5 +31,41 @@ onMounted(async () => {
 <style>
 .tweet {
   width: 550px;
+  padding: 2rem;
+  border: 1px solid rgb(234, 234, 234);
+  border-radius: 1rem;
+}
+.tweet-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.tweet-author {
+  display: flex;
+  align-items: center;
+}
+.tweet-author-image {
+  width: 48px;
+  height: 48px;
+  border-radius: 9999px;
+}
+.tweet-author-info {
+  margin-left: 1rem;
+}
+.tweet-author-name {
+  font-weight: 500;
+}
+.tweet-author-handler {
+  line-height: 1.4rem;
+  color: var(--primary);
+}
+.tweet-logo {
+  color: var(--primary);
+}
+.tweet-content {
+  margin-top: 1rem;
+}
+.tweet-content a {
+  color: var(--primary);
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ import { defineNuxtConfig } from "nuxt"
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["nuxt-windicss"],
+  css: ["~~/assets/main.css"],
 })
