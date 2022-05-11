319bb77965e05d105fd97b20e83a2ffb4ab9ee28
zernonia <zernonia@gmail.com>
2022-05-11 10:43:40 +0800

    remove crawler

--- server/api/tweet.ts
+++ server/api/tweet.ts
@@ -3,6 +3,6 @@ import { TweetOembed } from "~~/interface"
 export default defineEventHandler(async (event) => {
   const { url } = useQuery(event)
   const data = await $fetch<TweetOembed>(`https://publish.twitter.com/oembed?url=${url}`)
-  const result = await $fetch("/api/crawler", { method: "POST", body: data.html })
-  return { data, result }
+
+  return { data }
 })
