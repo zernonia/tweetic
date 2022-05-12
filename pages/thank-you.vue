0893baae6c33edf37da5fc538da0b8ebf3642ff2
zernonia <zernonia@gmail.com>
2022-05-12 21:46:06 +0800

    add thank you page

---
+++ pages/thank-you.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const tweets = ref([
+  "https://twitter.com/dominiksumer/status/1524741858009694209",
+  "https://twitter.com/maazarin/status/1524732072832774147",
+  "https://twitter.com/kecrily/status/1524665293540589569",
+  "https://twitter.com/pramit_armpit/status/1524637652565524481",
+  "https://twitter.com/ImnNazri/status/1524626328167010304",
+])
+
+useCustomHead("Thank you for your kind word! | Tweetic")
+</script>
+
+<template>
+  <div class="mb-32">
+    <h2 class="text-4xl md:text-6xl text-center font-semibold my-8 md:my-20">Thank you everyone!!</h2>
+
+    <MasonryWall :items="tweets" :column-width="400" :gap="10">
+      <template #default="{ item, index }">
+        <Tweet class="flex justify-center" :url="item" layout="supabase"></Tweet>
+      </template>
+    </MasonryWall>
+
+    <p class="text-center mt-8 md:mt-20">...and everyone who likes and retweet!</p>
+  </div>
+</template>
