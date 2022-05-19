7c2e086ba7b37f65b3cb80d0bb6dbce06c205a26
zernonia <zernonia@gmail.com>
2022-05-19 10:03:45 +0800

    add css highlight

--- plugins/highlight.ts
+++ plugins/highlight.ts
@@ -1,11 +1,13 @@
 import hljs from "highlight.js/lib/core"
 import json from "highlight.js/lib/languages/json"
 import html from "highlight.js/lib/languages/xml"
+import css from "highlight.js/lib/languages/css"
 import "highlight.js/styles/base16/github.css"
 
 export default defineNuxtPlugin(() => {
   hljs.registerLanguage("json", json)
   hljs.registerLanguage("html", html)
+  hljs.registerLanguage("css", css)
 
   return {
     provide: {
