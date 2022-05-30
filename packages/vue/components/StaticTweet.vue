7e4c6183b3f506498e2ca9204fa9213f4319494f
zernonia <zernonia@gmail.com>
2022-05-30 15:40:08 +0800

    add statictweet components

---
+++ packages/vue/components/StaticTweet.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { TweetOptions } from "~~/interface"
+import { useFetchStaticTweet } from "../composables/fetch"
+
+const props = defineProps({
+  url: String,
+  layout: { type: String, default: "" },
+  css: { type: String, default: "" },
+  show_original_link: { type: Boolean, default: false },
+  enable_twemoji: { type: Boolean, default: true },
+})
+
+const { url } = toRefs(props)
+const option = computed<TweetOptions>(() => ({
+  layout: props.layout,
+  css: props.css,
+  show_original_link: props.show_original_link,
+  enable_twemoji: props.enable_twemoji,
+}))
+
+const { data } = useFetchStaticTweet(url, option)
+</script>
+
+<template>
+  <div v-if="data?.html?.length" v-html="data.html"></div>
+</template>
---
+++ packages/vue/components/index.ts
@@ -0,0 +1,1 @@
+export { default as StaticTweet } from "./StaticTweet.vue"
