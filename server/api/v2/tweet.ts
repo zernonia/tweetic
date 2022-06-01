9c78d9917ccf652d7fc29a5f16f7a2d28bc7903d
zernonia <zernonia@gmail.com>
2022-06-01 13:45:01 +0800

    fix: add try catch to api

--- server/api/v2/tweet.ts
+++ server/api/v2/tweet.ts
@@ -5,16 +5,21 @@ export default defineEventHandler(async (event) => {
   const { url, layout, css, enable_twemoji, show_media } = useQuery(event)
   const id = url.toString().split("/")[5]
 
-  const options: TweetOptions = {
-    layout: layout?.toString(),
-    css: css?.toString(),
-    enable_twemoji: enable_twemoji ? JSON.parse(enable_twemoji.toString()) : false,
-    show_media: show_media ? JSON.parse(show_media.toString()) : false,
+  try {
+    const options: TweetOptions = {
+      layout: layout?.toString(),
+      css: css?.toString(),
+      enable_twemoji: enable_twemoji ? JSON.parse(enable_twemoji.toString()) : false,
+      show_media: show_media ? JSON.parse(show_media.toString()) : false,
+    }
+
+    const data = await $fetch<{ [key: string]: string }>(`https://syndication.twitter.com/tweets.json?ids=${id}`)
+
+    const tweetContent = getTweetContent(data, options)
+    const html = constructHtmlv2(tweetContent, options)
+    return { html, meta: tweetContent.meta }
+  } catch (err) {
+    event.res.statusCode = 400
+    return { err }
   }
-
-  const data = await $fetch<{ [key: string]: string }>(`https://syndication.twitter.com/tweets.json?ids=${id}`)
-
-  const tweetContent = getTweetContent(data, options)
-  const html = constructHtmlv2(tweetContent, options)
-  return { html, meta: tweetContent.meta }
 })
