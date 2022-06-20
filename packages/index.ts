98981097128acf44150d7b7e888b6ce1d52e9f83
zernonia <zernonia@gmail.com>
2022-06-20 23:38:13 +0800

    export fetchStaticTweet

--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,19 @@
-export * from "../utils/_lib"
+import { getSyndication, constructHtml } from "~~/utils/_lib"
+import { TweetOptions } from "~~/utils/types"
+
+const defaultOptions: TweetOptions = {
+  layout: "",
+  css: "",
+  enable_twemoji: true,
+  show_media: true,
+  show_quoted_tweet: true,
+  show_info: true,
+}
+export const fetchStaticTweet = async (tweetId: string, options = defaultOptions) => {
+  try {
+    const data = await getSyndication(tweetId)
+    return constructHtml(data, options)
+  } catch (err) {
+    throw new Error(`${err}`)
+  }
+}
