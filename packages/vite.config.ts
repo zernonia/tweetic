3f77fb2f59e952a54a1c9650ec08dedbf9e180db
zernonia <zernonia@gmail.com>
2022-06-20 23:16:29 +0800

    fix types

--- packages/vite.config.ts
+++ packages/vite.config.ts
@@ -4,7 +4,7 @@ import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [dts()],
   resolve: {
     alias: {
       "~~": path.resolve(__dirname, ".."),
